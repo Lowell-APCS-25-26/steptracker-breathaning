6ef8f9448da6adf619d964ae774d7ce6faab487d
breathaning <etzhu1@gmail.com>
2026-04-21 05:12:00 +0000

    submit

--- lib/src/main/java/org/APCSLowell/StepTracker.java
+++ lib/src/main/java/org/APCSLowell/StepTracker.java
@@ -1,8 +1,23 @@
 package org.APCSLowell;
 
 public class StepTracker {
-    public StepTracker(int activityThreshold) {}
-    void addDailySteps(int stepCount) {}
-    int activeDays() {}
-    double averageSteps() {}
+    private int min;
+    private int steps = 0;
+    private int activeDays = 0;
+    private int days = 0;
+    public StepTracker(int activityThreshold) {
+        this.min = activityThreshold;
+    }
+    void addDailySteps(int stepCount) {
+        steps += stepCount;
+        days ++;
+        if (stepCount >= min) activeDays++;
+    }
+    int activeDays() {
+        return activeDays;
+    }
+    double averageSteps() {
+        if (days == 0) return 0;
+        return (double)steps / (double)days;
+    }
 }
